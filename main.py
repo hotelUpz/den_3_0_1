89f71c644564bfb2daed6cd7b4d49132ce110a3b
kali <kali@kali.netis.cc>
2024-06-03 19:14:30 +0300

    is_terminal_only

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-# from tg_interfaces import TG_MANAGER
+from tg_interfaces import TG_MANAGER
 from main_logic import MAIN_CONTROLLER
+from SETTINGS import is_terminal_only 
 
 def go():
     try:
-        MAIN_CONTROLLER().main_func()
-        # print('Пожалуйста перейдите в интерфейс вашего телеграм бота!') 
-        # bot = TG_MANAGER()
-        # bot.run()
+        if is_terminal_only:
+            MAIN_CONTROLLER().main_func()
+        else:
+            print('Пожалуйста перейдите в интерфейс вашего телеграм бота!') 
+            bot = TG_MANAGER()
+            bot.run()
     except Exception as ex: 
         print(ex)
 if __name__=="__main__":
