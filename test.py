df7c1566fd0ea9f2ec671e45f54eec24f42cb6fe
kali <kali@kali.netis.cc>
2024-06-11 03:56:52 +0300

    del test.py

--- test.py
+++
@@ -1,3 +0,0 @@
-                        # random_tv_signal = choice(get_tv_signals_list)
-                        # symbol = random_tv_signal[0]
-                        # cur_signal = random_tv_signal[1]
